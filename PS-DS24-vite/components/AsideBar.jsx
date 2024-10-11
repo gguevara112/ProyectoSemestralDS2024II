ae7cdf16a68ea5e991ffcc9f1f76900f020bc67e
gguevara112 <guguevara@alumnos.uai.cl>
2024-10-11 09:23:47 -0300

    new commit

---
+++ PS-DS24-vite/components/AsideBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './AsideBar.css'; // Para los estilos
+
+const AsideBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`aside-bar ${isOpen ? 'open' : ''}`}>
+      <button className="toggle-button" onClick={toggleSidebar}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12" />
+        </svg>
+      </button>
+
+      <nav className="aside-content">
+        <ul>
+          <li className="menu-item">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="menu-icon">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+            Webiste traffic
+          </li>
+          <li className="menu-item">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="menu-icon">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+            Settings
+          </li>
+          <li className="menu-item">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="menu-icon">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+            Password
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default AsideBar;
