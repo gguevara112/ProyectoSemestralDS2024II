903cb06a9b43101e8bd8e64e6f9e345b231d7042
gguevara112 <guguevara@alumnos.uai.cl>
2024-10-13 14:19:25 -0300

    Header
    Solo el UI de el Header. falta el icono

--- PS-DS24-vite/src/pages/Home.jsx
+++ PS-DS24-vite/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-// src/pages/Home.jsx
 import React from 'react';
+import Header from '../components/Header'; // Importamos la barra de navegación
 
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page!</h1>
+      <Header /> {/* Añadimos la barra de navegación */}
       <h1>Welcome to the Home Page!</h1>
     </div>
   );
